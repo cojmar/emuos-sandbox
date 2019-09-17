23726d5452a3249dc7aa83ff60136d146d2d84e1
cojmar <cojmar@gmail.com>
2019-09-17 07:26:58 +0300

    basehref

--- app/preview.js
+++ app/preview.js
@@ -2,7 +2,15 @@ define(function(require) {
     var obj = {
         dom_object: require('./dom_object'),
         preview_head: require('text!assets/preview.html'),
-        preview_selector: false,
+        preview_selector: false,        
+        process_data:function(data){
+            if (!obj.url_params) obj.url_params = new URLSearchParams(window.location.search);
+            let ret = data;            
+            if(obj.url_params.has('baseurl')){
+                ret = ret.split('<head>').join('<head><base href="'+obj.url_params.get('baseurl')+'">');                
+            }             
+            return ret;
+        },
         set_preview: function(data) {
             if (!obj.preview_selector.html) return false;
             obj.preview_selector.html('<iframe id="preview_frame" style="width:100%;height:100%;border:none;" />');
@@ -10,7 +18,7 @@ define(function(require) {
             iframe = iframe.contentWindow || (iframe.contentDocument.document || iframe.contentDocument);
             iframe.document.open();
             iframe.document.write(obj.preview_head);
-            iframe.document.write(data);
+            iframe.document.write(obj.process_data(data));
             iframe.document.close();
 
         },
